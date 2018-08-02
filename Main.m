228f7a82f3e3c3bdec14c2661bf11d743631e399
Gabriel Sales Lins Rodrigues <gabriellins2@gmail.com>
2018-08-02 02:41:25 -0300

    Working version after issues with convergence and simulation structure

--- Main.m
+++ Main.m
@@ -40,11 +40,13 @@ AlternatorParametersEMS;
 
 % 
 e_a_str = regexprep(func2str(alternator.stator.input.e.function), '@\(.+?\)', '');
-replaceFileExpression('+SimscapeCustomBlocks/back_emf.ssc', 'e == 0', ['e == ' e_a_str]);
+replaceFileExpression('+SimscapeCustomBlocks/+Alternator/back_emf.ssc', ...
+    'e == 0', ['e == ' e_a_str]);
 
 % 
 l_a_str = regexprep(func2str(alternator.stator.l.function), '@\(.+?\)', '');
-replaceFileExpression('+SimscapeCustomBlocks/stator_inductance.ssc', 'l == 1e-6', ['l == ' l_a_str]);
+replaceFileExpression('+SimscapeCustomBlocks/+Alternator/stator_inductance.ssc', ...
+    'l == 1e-6', ['l == ' l_a_str]);
 
 %% Retificador
 
@@ -74,39 +76,13 @@ ice.n = ans.n_ice;
 
 % Alternador
 alternator.rotor.control.q = ans.q_s_f;
-alternator.rotor.d.v = timeseries(ans.d_f.Data(:, 1), ans.d_f.Time);
-alternator.rotor.d.i = timeseries(ans.d_f.Data(:, 2), ans.d_f.Time);
-alternator.rotor.s.v_gs = timeseries(ans.s_f.Data(:, 1), ans.s_f.Time);
-alternator.rotor.s.v_ds = timeseries(ans.s_f.Data(:, 2), ans.s_f.Time);
-alternator.rotor.s.i = timeseries(ans.s_f.Data(:, 3), ans.s_f.Time);
 alternator.rotor.l.i = ans.i_f;
 
 alternator.stator.input.e.value = ans.e_a_abc;
 alternator.stator.l.value = ans.l_a_abc;
-alternator.stator.l.v = ans.v_l_a_abc;
-alternator.stator.l.i = ans.i_a_abc;
-alternator.stator.r.v = ans.v_r_a_abc;
-alternator.stator.r.i = ans.i_a_abc;
 alternator.stator.output.v = ans.v_a_abc;
 
 % Retificador
-rectifier.d_1.v = timeseries(ans.d_r_1.Data(:, 1), ans.d_r_1.Time);
-rectifier.d_1.i = timeseries(ans.d_r_1.Data(:, 2), ans.d_r_1.Time);
-rectifier.d_2.v = timeseries(ans.d_r_2.Data(:, 1), ans.d_r_2.Time);
-rectifier.d_2.i = timeseries(ans.d_r_2.Data(:, 2), ans.d_r_2.Time);
-rectifier.d_3.v = timeseries(ans.d_r_3.Data(:, 1), ans.d_r_3.Time);
-rectifier.d_3.i = timeseries(ans.d_r_3.Data(:, 2), ans.d_r_3.Time);
-
-rectifier.s_1.v_gs = timeseries(ans.s_r_1.Data(:, 1), ans.s_r_1.Time);
-rectifier.s_1.v_ds = timeseries(ans.s_r_1.Data(:, 2), ans.s_r_1.Time);
-rectifier.s_1.i = timeseries(ans.s_r_1.Data(:, 3), ans.s_r_1.Time);
-rectifier.s_2.v_gs = timeseries(ans.s_r_2.Data(:, 1), ans.s_r_1.Time);
-rectifier.s_2.v_ds = timeseries(ans.s_r_2.Data(:, 2), ans.s_r_1.Time);
-rectifier.s_2.i = timeseries(ans.s_r_2.Data(:, 3), ans.s_r_1.Time);
-rectifier.s_3.v_gs = timeseries(ans.s_r_3.Data(:, 1), ans.s_r_1.Time);
-rectifier.s_3.v_ds = timeseries(ans.s_r_3.Data(:, 2), ans.s_r_1.Time);
-rectifier.s_3.i = timeseries(ans.s_r_3.Data(:, 3), ans.s_r_1.Time);
-
 rectifier.output.v = timeseries(ans.rectifier_output.Data(:, 1), ans.rectifier_output.Time);
 rectifier.output.i = timeseries(ans.rectifier_output.Data(:, 2), ans.rectifier_output.Time);
 
@@ -125,10 +101,12 @@ close_system('AutomotiveEMS.slx');
 %% 
 
 % 
-replaceFileExpression('+SimscapeCustomBlocks/back_emf.ssc', ['e == ' e_a_str], 'e == 0');
+replaceFileExpression('+SimscapeCustomBlocks/+Alternator/back_emf.ssc', ...
+    ['e == ' e_a_str], 'e == 0');
 
 %
-replaceFileExpression('+SimscapeCustomBlocks/stator_inductance.ssc', ['l == ' l_a_str], 'l == 1e-6');
+replaceFileExpression('+SimscapeCustomBlocks/+Alternator/stator_inductance.ssc', ...
+    ['l == ' l_a_str], 'l == 1e-6');
 
 %% Remoção do diretório principal e seus subdiretórios dos caminhos de 
 %  busca do MATLAB
