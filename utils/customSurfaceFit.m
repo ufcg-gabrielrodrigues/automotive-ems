9eb737f9e15a576a528a65bdd6d700074a767d26
Gabriel Sales Lins Rodrigues <gabriel.rodrigues@ee.ufcg.edu.br>
2019-02-01 15:15:50 -0300

    Create utility function to fit custom surface

---
+++ utils/customSurfaceFit.m
@@ -0,0 +1,28 @@
+function [fitresult, gof] = customSurfaceFit(x, y, z, f, fx, fy, fz, p0, varargin)
+    % 
+    if (nargin > 8)
+        plot_flag = varargin{1};
+    else
+        plot_flag = false;
+    end
+    
+    % 
+    [xData, yData, zData] = prepareSurfaceData(x, y, z);
+
+    % 
+    ft = fittype(f, 'independent', {fx, fy}, 'dependent', fz);
+    opts = fitoptions('Method', 'NonlinearLeastSquares');
+    opts.Display = 'Off';
+    opts.StartPoint = p0;
+
+    % 
+    [fitresult, gof] = fit([xData, yData], zData, ft, opts);
+
+    if (plot_flag == true)
+        % 
+        figure('Name', 'Custom Surface Fit');
+        h = plot( fitresult, [xData, yData], zData );
+        legend( h, 'Custom Surface Fit', 'z vs. x, y', 'Location', 'NorthEast' );
+        grid on
+    end
+end
