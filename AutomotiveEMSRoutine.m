8fffe6bc343a7f01cc994b5aacbd696fcb18e255
Gabriel Sales Lins Rodrigues <gabriel.rodrigues@ee.ufcg.edu.br>
2019-02-28 03:50:16 -0300

    Removes sine wave gain in ESC demodulation and readjusts gain. Adds variable gain in hybrid control and prepares script to execute cases in parallel and plot (still untested).

--- AutomotiveEMSRoutine.m
+++ AutomotiveEMSRoutine.m
@@ -40,6 +40,19 @@ k_e_local = ['k_e = ' k_e_str ';'];
 blockHandle = find(slroot, '-isa', 'Stateflow.EMChart', 'Path', 'AutomotiveEMS/Hybrid Load Matching Controller Scheme/Hybrid Load Matching Controller/Load Matching Switched-Mode Rectifier Controller/MATLAB Function');
 blockHandle.Script = strrep(blockHandle.Script, k_e_default_local, k_e_local);
 
+% Par�metros de esquema de controle h�brido
+hybrid_control_cases = [1.0 0.0; 0.0 1.0; 1.0 0.1];
+
+%% Parâmetros auxiliares para figuras
+
+[num_cases, ~] = size(hybrid_control_cases);
+
+% �?ndice de figuras
+figure_index = 0;
+
+% Cores
+colors = distinguishable_colors(num_cases + 1);
+
 %% Alternador
 
 % Corrente de excitação máxima
@@ -77,9 +90,24 @@ simulationParameters.StopTime = num2str(t_f);   % [s]
 % Salva mundanças feitas no modelo
 save_system('models/AutomotiveEMS.slx');
 
+%% Configuração dos casos de teste como entrada do modelo no Simulink
+
+for test_case_index = 1:num_cases
+    test_case = hybrid_control_cases(test_case_index, :);
+    
+    hybrid_control.k_p(test_case_index) = test_case(1);
+    hybrid_control.k_i(test_case_index) = test_case(2);
+    
+    simIn(test_case_index) = Simulink.SimulationInput('AutomotiveEMS');
+    simIn(test_case_index) = simIn(test_case_index).setBlockParameter('AutomotiveEMS/Hybrid Load Matching Controller Scheme/Hybrid Load Matching Controller/k_p', 'Value', num2str(hybrid_control.k_p(test_case_index)));
+    simIn(test_case_index) = simIn(test_case_index).setBlockParameter('AutomotiveEMS/Hybrid Load Matching Controller Scheme/Hybrid Load Matching Controller/k_i', 'Value', num2str(hybrid_control.k_i(test_case_index)));
+end
+
 %% Execução da simulação em ambiente Simulink
 
-simOut = sim('AutomotiveEMS', simulationParameters);
+% Execução da simulação paralelizada
+simOut = parsim(simIn, 'ShowProgress', 'on', 'ShowSimulationManager', 'on', ...
+    'TransferBaseWorkspaceVariables', 'on');
 
 %% Redefinição de parâmetros de alternador
 
@@ -92,36 +120,203 @@ blockHandle.Script = strrep(blockHandle.Script, k_e_local, k_e_default_local);
 save_system('models/AutomotiveEMS.slx');
 close_system('models/AutomotiveEMS.slx');
 
-%% Registro de resultados obtidos na simulação
+%% Registro de resultados obtidos no caso de teste
 
-% Motor a combustão interna
-ice.n = simOut.n_ice;
+% Laço de iterações por casos de teste
+for test_case_index = 1:num_cases
+    % Motor a combustão interna
+    ice.n(test_case_index) = simOut(test_case_index).n_ice;
+    
+    % Alternador
+    alternator.rotor.control.u(test_case_index) = simOut(test_case_index).u_i_f;
+    alternator.rotor.l.i(test_case_index) = simOut(test_case_index).i_f;
+    
+    alternator.stator.input.e(test_case_index) = simOut(test_case_index).e_a_abc;
+    alternator.stator.output.v(test_case_index) = simOut(test_case_index).v_a_abc;
+    alternator.stator.output.i(test_case_index) = simOut(test_case_index).i_a_abc;
+    
+    % Retificador
+    rectifier.control.u_lm(test_case_index) = simOut(test_case_index).u_lm;
+    rectifier.control.u_esc(test_case_index) = simOut(test_case_index).u_esc;
+    rectifier.control.u_smr(test_case_index) = simOut(test_case_index).u_smr;
+    
+    % Carga elétrica
+    electrical_load.v(test_case_index) = simOut(test_case_index).v_l;
+    electrical_load.i(test_case_index) = simOut(test_case_index).i_l;
+    electrical_load.p(test_case_index) = simOut(test_case_index).p_l;
+    
+    % Barramento de circuito secundário
+    bus.v(test_case_index) = simOut(test_case_index).v_bus;
+    bus.i(test_case_index) = simOut(test_case_index).i_bus;
+    bus.p(test_case_index) = simOut(test_case_index).p_bus;
+    
+    % Banco de supercapacitores
+    uc_bank.v(test_case_index) = simOut(test_case_index).v_uc;
+    uc_bank.i(test_case_index) = simOut(test_case_index).i_uc;
+    uc_bank.p(test_case_index) = simOut(test_case_index).p_uc;
+end
 
-% Alternador
-alternator.rotor.control.u = simOut.u_i_f;
-alternator.rotor.l.i = simOut.i_f;
+%% 
 
-alternator.stator.input.e = simOut.e_a_abc;
-alternator.stator.output.v = simOut.v_a_abc;
-alternator.stator.output.i = simOut.i_a_abc;
+% Carregamento de superf�cie ajustada de máxima potência
+try
+    load('results/PowerAnalysis/P_v_o.mat', 'P_v_o_mpp_sim_fit');
+catch
+    disp('Maximum power fit unavailable');
+end
 
-% Retificador
-rectifier.control.u = simOut.u_v_r;
+%% Análise de potência e ciclo de trabalho para cada esquema de controle
 
-% Carga elétrica
-electrical_load.v = simOut.v_l;
-electrical_load.i = simOut.i_l;
-electrical_load.p = simOut.p_l;
+% 
+figure_index = figure_index + 1;
+figure(figure_index)
 
-% Barramento de circuito secundário
-bus.v = simOut.v_bus;
-bus.i = simOut.i_bus;
-bus.p = simOut.p_bus;
+for test_case_index = 1:num_cases
+    plot(uc_bank.p(test_case_index), 'Color', colors(test_case_index, :), ...
+        'DisplayName', ['$k_{p} = ' num2str(hybrid_control.k_p(test_case_index)) '$; $k_{i} = ' num2str(hybrid_control.k_i(test_case_index)) '$']);
+    hold on;
+end
 
-% Banco de supercapacitores
-uc_bank.v = simOut.v_uc;
-uc_bank.i = simOut.i_uc;
-uc_bank.p = simOut.p_uc;
+if (exist('P_v_o_mpp_sim_fit', 'var'))
+    P_v_o_mpp = P_v_o_mpp_sim_fit(iceToAltRotRatio*ice.n(test_case_index).Data, alternator.rotor.l.i(test_case_index).Data);
+    P_v_o_mpp = timeseries(P_v_o_mpp, ice.n(test_case_index).Time);
+    
+    plot(P_v_o_mpp, 'Color', colors(num_cases + 1, :), 'DisplayName', 'Pot{\^{e}}ncia m{\''{a}}xima');
+end
+
+hold off;
+legend('off');
+legend('show');
+
+title('Pot{\^{e}}ncia transferida ao banco de supercapacitores');
+xlabel('$t$ $[s]$');
+ylabel('$P_{uc}$ $[W]$');
+leg = legend;
+leg.Location = 'NorthEast';
+grid on;
+
+% 
+figure_index = figure_index + 1;
+figure(figure_index)
+
+for test_case_index = 1:num_cases
+    plot(ice.n(test_case_index).Time, ice.n(test_case_index).Data*iceToAltRotRatio, 'Color', colors(test_case_index, :), ...
+        'DisplayName', ['$k_{p} = ' num2str(hybrid_control.k_p(test_case_index)) '$; $k_{i} = ' num2str(hybrid_control.k_i(test_case_index)) '$']);
+    hold on;
+end
+
+hold off;
+legend('off');
+legend('show');
+
+title('Velocidade do alternador');
+xlabel('$t$ $[s]$');
+ylabel('$n_{r}$ $[rpm]$');
+leg = legend;
+leg.Location = 'NorthEast';
+grid on;
+
+% 
+figure_index = figure_index + 1;
+figure(figure_index)
+
+for test_case_index = 1:num_cases
+    plot(alternator.rotor.l.i(test_case_index), 'Color', colors(test_case_index, :), ...
+        'DisplayName', ['$k_{p} = ' num2str(hybrid_control.k_p(test_case_index)) '$; $k_{i} = ' num2str(hybrid_control.k_i(test_case_index)) '$']);
+    hold on;
+end
+
+hold off;
+legend('off');
+legend('show');
+
+title('Corrente de excita{\c{c}}{\~{a}}o do alternador');
+xlabel('$t$ $[s]$');
+ylabel('$i_{f}$ $[A]$');
+leg = legend;
+leg.Location = 'NorthEast';
+grid on;
+
+% 
+figure_index = figure_index + 1;
+figure(figure_index)
+
+for test_case_index = 1:num_cases
+    plot(bus.v(test_case_index), 'Color', colors(test_case_index, :), ...
+        'DisplayName', ['$k_{p} = ' num2str(hybrid_control.k_p(test_case_index)) '$; $k_{i} = ' num2str(hybrid_control.k_i(test_case_index)) '$']);
+    hold on;
+end
+
+hold off;
+legend('off');
+legend('show');
+
+title('Tens{\~{a}}o de barramento do circuito secund{\''{a}}rio');
+xlabel('$t$ $[s]$');
+ylabel('$v_{dc}$ $[V]$');
+leg = legend;
+leg.Location = 'NorthEast';
+grid on;
+
+% 
+figure_index = figure_index + 1;
+figure(figure_index)
+
+subplot(3, 1, 1)
+for test_case_index = 1:num_cases
+    plot(rectifier.control.u_lm(test_case_index), 'Color', colors(test_case_index, :), ...
+        'DisplayName', ['$k_{p} = ' num2str(hybrid_control.k_p(test_case_index)) '$; $k_{i} = ' num2str(hybrid_control.k_i(test_case_index)) '$']);
+    hold on;
+end
+
+hold off;
+legend('off');
+legend('show');
+
+title('Sinal de controle baseado em modelo');
+xlabel('$t$ $[s]$');
+ylabel('$u$');
+leg = legend;
+leg.Location = 'NorthEast';
+grid on;
+
+subplot(3, 1, 2)
+for test_case_index = 1:num_cases
+    plot(rectifier.control.u_esc(test_case_index), 'Color', colors(test_case_index, :), ...
+        'DisplayName', ['$k_{p} = ' num2str(hybrid_control.k_p(test_case_index)) '$; $k_{i} = ' num2str(hybrid_control.k_i(test_case_index)) '$']);
+    hold on;
+end
+
+hold off;
+legend('off');
+legend('show');
+
+title('Sinal de controle baseado em otimiza{\c{c}}{\~{a}}o');
+xlabel('$t$ $[s]$');
+ylabel('$u$');
+leg = legend;
+leg.Location = 'NorthEast';
+grid on;
+
+subplot(3, 1, 1)
+for test_case_index = 1:num_cases
+    plot(rectifier.control.u_smr(test_case_index), 'Color', colors(test_case_index, :), ...
+        'DisplayName', ['$k_{p} = ' num2str(hybrid_control.k_p(test_case_index)) '$; $k_{i} = ' num2str(hybrid_control.k_i(test_case_index)) '$']);
+    hold on;
+end
+
+hold off;
+legend('off');
+legend('show');
+
+title('Sinal de controle resultante');
+xlabel('$t$ $[s]$');
+ylabel('$u$');
+leg = legend;
+leg.Location = 'NorthEast';
+grid on;
+
+suptitle('Sinais de controle de retificador');
 
 %% Armazenamento dos resultados de simulação
 
@@ -132,3 +327,4 @@ save('results/AutomotiveEMS/battery.mat', 'battery', '-v7.3');
 save('results/AutomotiveEMS/electrical_load.mat', 'electrical_load', '-v7.3');
 save('results/AutomotiveEMS/bus.mat', 'bus', '-v7.3');
 save('results/AutomotiveEMS/uc_bank.mat', 'uc_bank', '-v7.3');
+save('results/AutomotiveEMS/hybrid_control.mat', 'hybrid_control', '-v7.3');
